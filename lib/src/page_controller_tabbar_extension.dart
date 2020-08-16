0c6df4c3b32b330f6c1581d31ec5ba756542a8c6
David Araujo <davidsdearaujo@gmail.com>
2020-08-15 21:15:33 -0300

    warning solved

--- lib/src/page_controller_tabbar_extension.dart
+++ lib/src/page_controller_tabbar_extension.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/widgets.dart';
 
 extension TabbarPageControllerExtension on PageController {
-  @override
   double get page => positions.isEmpty ? 0 : page;
 
   bool get isInitialized => positions.isNotEmpty;
